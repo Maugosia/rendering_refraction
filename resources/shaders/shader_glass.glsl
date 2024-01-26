4b77ef988cf636845535dc91c22e972e425c30bb
Maugosia <maugorzata.sikora@gmail.com>
2024-01-26 14:19:40 +0100

    clear code

--- resources/shaders/shader_glass.glsl
+++ resources/shaders/shader_glass.glsl
@@ -6,10 +6,8 @@
 const float Air = 1.0;
 const float Glass = 1.51714;
 
-// Air to glass ratio of the indices of refraction (Eta)
 const float Eta = Air / Glass;
 
-// see http://en.wikipedia.org/wiki/Refractive_index Reflectivity
 const float R0 = ((Air - Glass) * (Air - Glass)) / ((Air + Glass) * (Air + Glass));
 
 uniform mat4 MVP;
@@ -20,9 +18,8 @@ uniform vec4 u_camera;
 in vec4 in_position;
 in vec3 in_normal;
 
-out vec3 v_reflection;
 out vec3 v_refraction;
-out float v_fresnel;
+
 
 void main(void)
 {
@@ -34,10 +31,6 @@ void main(void)
 	// Assume incoming normal is normalized.
 	vec3 normal = u_normalMatrix*in_normal;
 	v_refraction = refract(incident, normal, Eta);
-	v_reflection = reflect(incident, normal);
-
-	// see http://en.wikipedia.org/wiki/Schlick%27s_approximation
-	v_fresnel = R0 + (1.0 - R0) * pow((1.0 - dot(-incident, normal)), 5.0);
 
 
 }
@@ -47,16 +40,12 @@ void main(void)
 uniform samplerCube u_cubemap;
 
 in vec3 v_refraction;
-in vec3 v_reflection;
-in float v_fresnel;
 
 out vec4 fragColor;
 
 void main(void)
 {
 	vec4 refractionColor = texture(u_cubemap, normalize(v_refraction));
-	vec4 reflectionColor = texture(u_cubemap, normalize(v_reflection));
-	fragColor = mix(refractionColor, reflectionColor, v_fresnel);
 	fragColor = refractionColor;
 
 }
